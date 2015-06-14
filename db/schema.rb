7c028334ac234e68df66e1851798ec0e0226f0e0
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-06-14 16:17:26 -0400

    Finish Ch 9 user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,11 @@ ActiveRecord::Schema.define(version: 20150605234034) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
